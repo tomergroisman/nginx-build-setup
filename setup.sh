f21bfea8f558644684969a682730e7b6381c1eb0
Tomer Groisman <tomergroisman@gmail.com>
2021-09-19 12:36:13 +0300

    Fix build

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ tar -zxvf nginx-source.tar.gz -C nginx-source --strip-components=1
 cd nginx-source
 
 echo "Installing compilation dependencies" && sleep 1
-apt-get install libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev -y
+apt-get install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev -y
 
 echo "Compile nginx with custom configuration" && sleep 1
 ./configure --sbin-path=/usr/bin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --with-pcre --pid-path=/var/run/nginx.pid --with-http_ssl_module
@@ -31,7 +31,7 @@ make
 
 echo "Installing nginx build" && sleep 1
 make install
-clear
+cd ..
 
 echo "Verify installation files" && sleep 1
 ls -l /etc/nginx/
@@ -39,5 +39,8 @@ nginx -V
 
 echo "Setting systemd configuration" && sleep 1
 cp nginx.service /lib/systemd/system/nginx.service
+
+echo "Deleting installation files" && sleep 1
+rm -rf nginx-source*
 
 echo "done."
